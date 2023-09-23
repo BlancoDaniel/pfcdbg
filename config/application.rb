2a0c25510141dff5556130b2c01ab5f973e59377
Daniel Blanco <daniblangabe@gmail.com>
2023-09-23 14:40:34 +0200

    PPDB-7 se añade MVC Category + traducciones

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module Pfcdbg
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    #available languages
+    config.i18n.available_locales = :es
+
+    #language default
+    config.i18n.default_locale= :es
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
   root 'main#home'
+  resources :categories, except: :show
   devise_for :users
 end
