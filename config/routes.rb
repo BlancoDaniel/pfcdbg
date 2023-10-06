d57dd966b16f0d72ac7d4424df293b0071f560bd
Daniel Blanco <daniel.blanco@softwhisper.es>
2023-10-06 13:14:46 +0200

    PPDB-19 Estilos new/edit event

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
   resources :categories, except: :show
   resources :clients, only: [:new, :create]
-  resources :promoters, only: [:new, :create]
+  resources :promoters, only: [:show, :new, :create], path: '/promoter', param: :id
   resources :events
 end
