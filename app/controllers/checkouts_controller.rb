b255c730d61d7af267c67650f39a7e381f3c4b0f
Daniel Blanco <daniel.blanco@softwhisper.es>
2023-10-18 16:50:04 +0200

    PPDB-25 Controlar cantidad entradas compradas

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -2,9 +2,15 @@ class CheckoutsController < ApplicationController
 
   def show
     event
+    @inputQuantity = params[:inputQuantity].to_i
+    return redirect_to event_path(event), alert: "No hay tantas entradas disponibles"  if @inputQuantity>event.ticket_quantity 
+    return redirect_to event_path(event), alert: "Solo se permite la compra de 6 entradas"  if @inputQuantity>6
+
+    event.update(ticket_quantity: @inputQuantity)
     current_user.set_payment_processor :stripe
     current_user.payment_processor.customer
-    @inputQuantity = params[:inputQuantity].to_i
+
+  
     @total = @inputQuantity * event.price
 
     @checkout_session = current_user.payment_processor.checkout(
@@ -19,9 +25,13 @@ class CheckoutsController < ApplicationController
                      },
                      quantity: @inputQuantity
                    }],
-        success_url: "http://127.0.0.1:3000/events/#{event.id}",
+        success_url: checkout_success_url(id: event.id, qty: @inputQuantity),
         cancel_url: 'http://127.0.0.1:3000/events'
       )
+       
+  end
+
+  def success
 
   end
 
@@ -30,5 +40,7 @@ class CheckoutsController < ApplicationController
   def event
     @event = Event.find(params[:id])
   end
-end
 
+
+
+end
