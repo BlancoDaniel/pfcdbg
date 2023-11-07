1af6b60eb1ddbc54c5d5b6de714be5cf0e8f3087
Daniel Blanco <daniel.blanco@softwhisper.es>
2023-11-07 10:29:02 +0100

    PPDB-28 Main

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,9 @@
 class MainController < ApplicationController
-  def home
+
+  def index
+    @events = FindEvents.new.upcoming_events.load_async
+    @latest_events = FindEvents.new.latest_events.load_async
+    @latest_tickets = FindEvents.new.latest_tickets.load_async
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'main#home'
+  root 'main#index'
   devise_for :users, controllers: { registrations: "registrations" }
   resources :categories, except: :show
   resources :clients, only: [:new, :create]
